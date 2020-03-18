ae30201707430559418efc21c805400737a500f8
bmend <bmend@Mendanha>
2020-03-18 15:50:30 +0000

    test of message

--- src/dorset2/Dorset2.java
+++ src/dorset2/Dorset2.java
@@ -18,6 +18,14 @@ public class Dorset2 {
         // TODO code application logic here
         
         // Class 18/03
+       /*Write a method that takes a four digit number and returns the sum of the digits
+        E.g 7631 = 17; 5555 = 20Hints:
+        You need to convert the number to a string
+        You need to use a loop
+        You need to create a running total
+        You need to extract each character in a string and convert it to a number
+        Start by creating a loop that just creates a running total based on the counter of the loop
+*/
         
     }
     
